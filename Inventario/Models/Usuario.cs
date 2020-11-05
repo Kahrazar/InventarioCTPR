8bf725fba8c95eb06fdbff23b83f33003dfe4d6e
TheApeMaster <oficialgadea@gmail.com>
2020-11-05 15:52:32 -0600

    LoginUpdateCheckpoint

---
+++ Inventario/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [Required]
+        [StringLength(10)]
+        public string IdCedula { get; set; }
+        [Required]
+        public string Contraseña { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+    }
+}
