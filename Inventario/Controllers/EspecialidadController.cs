2a9f2c9e7a7b2df58483f59de58b1f0a6a647593
Diego <diegoula08@hotmail.com>
2020-11-12 12:24:33 -0600

    Inicio del desarrollo de las vista de la especialidad

---
+++ Inventario/Controllers/EspecialidadController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventario.Services;
+using Inventario.Models;
+
+namespace Inventario.Controllers
+{
+    public class EspecialidadController : Controller
+    {
+        EspecialidadRepository repositorio = new EspecialidadRepository();   
+        // GET: Especialidad
+        public ActionResult VerEspecialidades()
+        {
+            return View(repositorio.obtenerEspecialidades());
+        }
+
+        // GET: Especialidad/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Especialidad/Create
+        public ActionResult AgregarEspecialidad()
+        {
+            return View();
+        }
+
+        // POST: Especialidad/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AgregarEspecialidad(Especialidad collection)
+        {
+           collection.ID = repositorio.generarID();
+            repositorio.anadirEspecialidad(collection);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+
+                    return RedirectToAction("VerEspecialidades");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+
+        }
+
+        // GET: Especialidad/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Especialidad/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Especialidad/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Especialidad/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Inventario/Services/EspecialidadRepository.cs
@@ -0,0 +1,49 @@
+﻿using Inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventario.Services
+{
+    public class EspecialidadRepository
+    {
+        public int generarID()
+        {
+            int id;
+            List<Especialidad> lista = obtenerEspecialidades();
+
+            id = lista.Count;
+            id = id + 2;
+            return id;
+        }
+
+        public void anadirEspecialidad(Especialidad especialidad)
+        {
+            using (var db = new ApplicationDbContext())
+                try
+                {
+                    db.Especialidad.Add(especialidad);
+                    db.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+        }
+        public List<Especialidad> obtenerEspecialidades()//Este metodo extrae las especialiadades de la base de datos
+        {
+            using (var db = new ApplicationDbContext())
+                try
+                {
+                    return db.Especialidad.ToList();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+        }
+    }
+}
