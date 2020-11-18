4eb99508c8067914ee0feb5355503c0395f513df
Kahrazar <73491485+Kahrazar@users.noreply.github.com>
2020-11-18 16:04:45 -0600

    Validacion Lista

--- Inventario/Scripts/validar.js
+++ Inventario/Scripts/validar.js
@@ -1,16 +1,21 @@
 ﻿//const formulario = document.getElementById("formulario");
 //const inputs = document.querySelectorAll("#formulario input");
-const btn = document.getElementById("anadir");
+var prueba = document.getElementById("Validar");
 const campoPatrimonio = document.getElementById("numeroDePatrimonio");
 const expresiones = {
-    patrimonio: /(^[A-Z]{3}[0-9]+$|\d{5,9})/,
+    patrimonio: /(^[A-Z]{3}[0-9]{3}$)|(^\d{9}$)/,
     barras: /\d{9}/,
     ubicacion: /^[A-Z]-[0-9]+/
 }
-btn.disabled = false;
 const comprobar = (e) => {
-    if (expresiones.patrimonio.test()) {
-
+    if (expresiones.patrimonio.test(campoPatrimonio.value)) {
+        document.getElementById("buscar").disabled = false;
+        prueba.classList.add("d-none");
+    } else
+    {
+ 
+        document.getElementById("buscar").disabled = true;
+        prueba.classList.remove("d-none");
     }
 }
 
---
+++ Inventario/Scripts/validaranadir.js
@@ -0,0 +1,33 @@
+﻿const campoPatrimonio = document.getElementById("numeroDePatrimonio");
+const campoCodigo = document.getElementById("codigoDeBarras");
+const campoUbicacion = document.getElementById("ubicacion");
+
+const expresiones = {
+    patrimonio: /(^[A-Z]{3}[0-9]{3}$)|(^\d{9}$)/,
+    barras: /^\d{9}/,
+    ubicacion: /^[A-Z]-[0-9]$/
+}
+const comprobar = (e) => {
+    if (expresiones.patrimonio.test(campoPatrimonio.value)) {
+        if (expresiones.barras.test(campoCodigo.value)) {
+            if (expresiones.ubicacion.test(campoUbicacion.value)) {
+                document.getElementById("anadir").disabled = false;
+            } else {
+                document.getElementById("anadir").disabled = true; 
+            }
+        } else {
+            document.getElementById("anadir").disabled = true;
+        }
+    } else{
+        document.getElementById("anadir").disabled = true;
+    }
+}
+
+campoPatrimonio.addEventListener('keyup', comprobar);
+campoPatrimonio.addEventListener('blur', comprobar);
+
+campoCodigo.addEventListener('blur', comprobar);
+campoCodigo.addEventListener('keyup', comprobar);
+
+campoUbicacion.addEventListener('blur', comprobar);
+campoUbicacion.addEventListener('keyup', comprobar);
